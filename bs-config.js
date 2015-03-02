899e180322aa53f6b7c15a8cc32f076834c99fba
Josh Ponelat <josh@ponelat.com>
2015-03-02 18:05:27 +0200

    initial commit, h5bp + compass

---
+++ bs-config.js
@@ -0,0 +1,49 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "files": "*.html,\
+              *.js,\
+              js/*.js,\
+              css/*.css",
+    "server": true,
+    "proxy": false,
+    "port": 3000,
+    "ghostMode": false,
+    "logLevel": "info",
+    "logPrefix": "BS",
+    "logConnections": false,
+    "logFileChanges": true,
+    "logSnippet": true,
+    "open": "local",
+    "browser": "default",
+    "xip": false,
+    "hostnameSuffix": false,
+    "notify": true,
+    "scrollProportionally": true,
+    "scrollThrottle": 0,
+    "reloadDelay": 0,
+    "injectChanges": true,
+    "startPath": "",
+    "minify": false,
+    "host": null,
+    "codeSync": true,
+    "timestamps": true,
+    "socket": {
+        "path": "/browser-sync/socket.io",
+        "clientPath": "/browser-sync",
+        "namespace": "/browser-sync"
+    },
+    "debugInfo": true
+};
